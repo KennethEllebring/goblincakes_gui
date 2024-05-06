814abacbc3039ef4145b7b338e57add5e865c8cb
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2024-05-06 13:19:22 +0200

    added password change possibility and userProfile page

--- src/pages/dashboard/userpages/UserProfile.tsx
+++ src/pages/dashboard/userpages/UserProfile.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useAuth } from "../../../components/auth/authContext";
 
 const UserProfile = () => {
+    const { username } = useAuth();
     return (
         <div>
-            <h1>Profil</h1>
+            <h1>{`Välkommen ${username}!`}</h1>
+            <span>
+                OBS! innan du fortsätter, gå in på inställningar och ändra ditt
+                lösenord
+            </span>
         </div>
     );
 };
--- src/pages/dashboard/userpages/UserSettings.tsx
+++ src/pages/dashboard/userpages/UserSettings.tsx
@@ -1,10 +1,103 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import { useAuth } from "../../../components/auth/authContext";
+import { fetchData } from "../../../utils/api";
+import { toast } from "react-toastify";
 
 const Settings = () => {
+    const { username } = useAuth();
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (newPassword !== confirmNewPassword) {
+            setErrorMessage("New passwords do not match.");
+            return;
+        }
+
+        try {
+            const response = await fetchData(
+                "http://localhost:5050/api/auth/change",
+                {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    credentials: "include",
+                    body: JSON.stringify({
+                        username: username,
+                        oldPassword: oldPassword,
+                        newPassword: newPassword,
+                    }),
+                },
+            );
+
+            setOldPassword("");
+            setNewPassword("");
+            setConfirmNewPassword("");
+            setErrorMessage("");
+
+            console.log("Password updated successfully: ", response);
+            toast.success("Password updated successfully");
+        } catch (error) {
+            console.error("Kunde inte uppdatera lösenord: ", error);
+            toast.error("Kunde inte uppdatera lösenord");
+            setErrorMessage(
+                "Kunde inte uppdatera, kontrollera lösenord och försök igen",
+            );
+        }
+    };
+
     return (
-        <div>
-            <h1>Inställningar</h1>
-        </div>
+        <>
+            <div className="settings-password">
+                <h2>Ändra lösenord</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="oldPassword">Gammalt lösenord:</label>
+                        <input
+                            type="password"
+                            id="oldPassword"
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="newPassword">Nytt lösenord:</label>
+                        <input
+                            type="password"
+                            id="newPassword"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="confirmNewPassword">
+                            Bekräfta nytt lösenord:
+                        </label>
+                        <input
+                            type="password"
+                            id="confirmNewPassword"
+                            value={confirmNewPassword}
+                            onChange={(e) =>
+                                setConfirmNewPassword(e.target.value)
+                            }
+                            required
+                        />
+                    </div>
+                    {errorMessage && (
+                        <div style={{ color: "red" }}>{errorMessage}</div>
+                    )}
+                    <button type="submit" className="update-button">
+                        Uppdatera lösenord
+                    </button>
+                </form>
+            </div>
+        </>
     );
 };
 
